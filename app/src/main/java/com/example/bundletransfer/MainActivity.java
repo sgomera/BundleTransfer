efdd82d6a43872e0f1fe883ab21f53632e881e7a
sgomera <silviagomera@gmail.com>
2019-06-11 11:27:22 +0200

    Bundle transfer

--- app/src/main/java/com/example/bundletransfer/MainActivity.java
+++ app/src/main/java/com/example/bundletransfer/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void clickSumar(View view){
+
+        //creem les instàncies de les vistes de text dels num1 i num2, de tipus EditText:
+        EditText quadre1 = (EditText) findViewById(R.id.num1);
+        EditText quadre2 = (EditText)findViewById(R.id.num2);
+        //el que es guarda dins de EditText sempre és string.
+
+        int n1 = Integer.parseInt(quadre1.getText().toString());
+        int n2 = Integer.parseInt(quadre2.getText().toString());
+
+
         Intent intent = new Intent(this, Sumar.class);
+
+        //per traspassar dades a la classe Sumar
+        //de moment passem dades fixes, en un futur seran els de l'usuari
+        intent.putExtra("number1", n1);
+        intent.putExtra("number2", n2);
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/bundletransfer/Sumar.java
+++ app/src/main/java/com/example/bundletransfer/Sumar.java
@@ -2,11 +2,26 @@ package com.example.bundletransfer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Sumar extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultat);
+
+        //hem de rescatar les dades del bundle.
+        Bundle dades = getIntent().getExtras();
+
+        int num1 = dades.getInt("number1");
+        int num2 = dades.getInt("number2");
+
+        //fem el càlcul
+        int resultat = num1 + num2;
+
+        //mostrem el resultat en la vista corresponent
+        TextView textView = (TextView)findViewById(R.id.textResultat);
+        textView.setText(""+ resultat);
+
     }
 }
